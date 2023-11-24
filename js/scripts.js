2c2f047e0f80265c3cfd5653705261d4722e5bf5
Cherilene Payongayong <payongayong@uleth.ca>
2023-11-23 23:10:18 -0700

    creation

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// console.log('connected!');
+// var x = document.title;
+// console.log(x);
+
+function changeImage(newImage) {
+    var imageElement = document.getElementById("myImage");
+    // Change the image source to the new image
+    imageElement.src = newImage;
+}
